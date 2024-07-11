c595763eb7b63cf2ad85409ae56aa4a6a7fd965d
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2024-07-11 16:48:05 +0200

    Creacion de constructor y uso de los mismos.

--- Curso_Poo_Java(paso_a_paso)/src/Logica/Alumno.java
+++ Curso_Poo_Java(paso_a_paso)/src/Logica/Alumno.java
@@ -4,25 +4,41 @@ package Logica;
 //Creamos la clase dentro del paquete logica
 public class Alumno {
 
-    //creamos los atributos de la clase.
+    //Creamos los atributos de la clase (suelen ser variable).
     int id;
     String nombre;
     String apellido;
 
-    //Asignamos sus METODOS o Acciones (o cosas que puede hacer siempre son verbos en infinitivo).
+
+    //Creamos sus METODOS o Acciones (cosas que puede hacer siempre son verbos en infinitivo).
     //ejemplo: mostrar su nombre(mostrarNombre) y saber si a aprobado(saberAprovado).
 
+    //Metodo de ProcedimientoJ(que no nos devuelve ningun valor)
     public void mostrarNombre () {
         System.out.println("Hola, soy un alumno y se decir mi nombre");
     }
-    
+
+    //Metodo de Funcion (que devulve un valor especifico)
     public void  sabreAprovado(double calificacion){
         if (calificacion>=6) {
             System.out.println("Aprobé la materia");
         }
         else {
             System.out.println("Uyy no aprobé");
+        }
+
+    }
+    //Creamos un constructor con el atajoj(Alt Insert) constructor y no seleccionamos ningun atributo.
+    public Alumno() {
     }
 
+    //Creamos el mismo constructor pero seleccionando todos los atributos
+    public Alumno(int id, String nombre, String apellido) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
 
+
+    }
+    //Luego los llamamos en el main.
 }
---
+++ Curso_Poo_Java(paso_a_paso)/src/Logica/Main.java
@@ -0,0 +1,12 @@
+package Logica;
+
+public class Main {
+    public static void main(String[] args) {
+        // LLamamos al nombre de la clase del objeto como si fuera una variable
+        // Ejemplo sin atributos
+        Alumno alu1 = new Alumno();
+
+        // Ejemplo con atributos
+        Alumno alu2 = new Alumno(5,"Andy","Gómez");
+    }
+}
