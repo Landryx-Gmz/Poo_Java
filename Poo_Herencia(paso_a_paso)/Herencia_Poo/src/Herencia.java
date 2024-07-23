128b4fed8a12ca3ae44d9449d428645e641d5728
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2024-07-23 15:16:55 +0200

    Polimorfismo, ejmplo y apunte

--- Poo_Herencia(paso_a_paso)/Herencia_Poo/src/Herencia.java
+++ Poo_Herencia(paso_a_paso)/Herencia_Poo/src/Herencia.java
@@ -1,12 +1,13 @@
 public class Herencia extends Persona{
     public static void main(String[] args) {
-        Empleado emple = new Empleado();
-
-        emple.getNum_legajo();
-        emple.getNombre();
-
-        Consultor consul1 = new Consultor();
-        consul1.getNum_consultor();
-        consul1.getNombre();
+        //Ejemplo de polimorfismo
+    Persona vector [] = new Persona[5];
+    //En el vector de la clase padre(Persona) vemos como podemos asiganar todas sus clases Hijas
+    //Con ello se demuestra su Polimorfismo es decir todas las clases con sus atibutos heredadas
+    //Y personalizadas.
+    vector[0] = new Persona();
+    vector[1] = new Empleado();
+    vector[2] = new Consultor();
+    vector[3] = new Jefe();
     }
 }
